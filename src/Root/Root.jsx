0b075a3d68928f2130dcd7f3e365c2a28d30caf5
imsusantosarker <susantosarkermail@gmail.com>
2024-01-08 00:55:19 +0600

    react-router-tailwind-daisyui-setup

---
+++ src/Root/Root.jsx
@@ -0,0 +1,8 @@
+
+export const Root = () => {
+  return (
+    <div className="">
+        <h1>Root Component</h1>
+    </div>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import { Root } from './Root/Root';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children:[
+      {
+        
+      }
+    ]
+  },
+]);
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
