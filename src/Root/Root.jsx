2438f10147f34027f6dbb7869efbfa37f5cd40f0
imsusantosarker <susantosarkermail@gmail.com>
2024-01-09 01:00:56 +0600

    footer Component

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom"
 import { Header } from "../component/Header/Header"
+import { Footer } from "../component/Footer/Footer"
 
 export const Root = () => {
   return (
     <div className="">
         <Header></Header>
         <Outlet></Outlet>
+        <Footer></Footer>
     </div>
   )
 }
